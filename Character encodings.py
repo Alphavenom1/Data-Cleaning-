1f27cb200e4ffd0d0a204d01ad3d210a6410f21e
nouhayla <94287848+Alphavenom1@users.noreply.github.com>
2022-07-08 01:16:31 +0200

    Create Character encodings.py

---
+++ Character encodings.py
@@ -0,0 +1,10 @@
+#create a variable new_entry that changes the encoding from "big5-tw" to "utf-8". new_entry should have the bytes datatype.
+new_entry = (sample_entry.decode("big5-tw")).encode('utf-8', errors="replace")
+
+#Use the code cell below to read in this file at path "../input/fatal-police-shootings-in-the-us/PoliceKillingsUS.csv".
+with open("../input/fatal-police-shootings-in-the-us/put isPoliceKillingsUS.csv", 'rb') as rawdata:
+    result = chardet.detect(rawdata.read(10000))
+print(result)
+#output is : {'encoding': 'ascii', 'confidence': 1.0, 'language': ''}
+police_killings=pd.read_csv("../input/fatal-police-shootings-in-the-us/PoliceKillingsUS.csv", encoding='ascii')
+police_killings.head()
